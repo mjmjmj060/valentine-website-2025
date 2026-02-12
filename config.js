17667b74b26663981cce6c1760c506a738ab1a70
mjmjmj060 <mjmjmj060@gmail.com>
2026-02-12 23:58:13 +0100

    Update love meter messages and celebration text

--- config.js
+++ config.js
@@ -41,15 +41,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOW, you love me more than 3900%!? 🚀💝",  // Shows when they go past 5000%
+        high: ":)))",              // Shows when they go past 1000%
+        normal: ":)"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now coccole time!",
+        title: "Yay! Now COCCOLE TIME!",
+        message: "!!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
